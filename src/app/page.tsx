4f4edcff93b1eff0b08e898718d2a1e29eb91c0d
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-09-27 11:54:08 -0400

    added test line to source code (seeing if gcloud builds image)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <section className="py-24">
       <div>
         <h1 className="text-3xl font-bold">Project Pulse yuhhhhh</h1>
+        <h1 className="text-3xl font-bold">Sussy</h1>
       </div>
     </section>
   )
