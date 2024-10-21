dc7d776fb294be40b36e5aef7298a8bfbaf13421
Alex Miller <amiller06880@gmail.com>
2024-10-21 17:27:56 -0400

    whenever emily sends me the logo I will add it to this branch

---
+++ src/app/_app.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
