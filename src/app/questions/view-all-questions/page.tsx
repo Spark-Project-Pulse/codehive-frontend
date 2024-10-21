7952240ec7a064f38da9d8170e47388b4432c39f
Aidan <aidsc@bu.edu>
2024-10-21 14:49:11 -0400

    reflecting mensuns changes

--- src/app/questions/view-all-questions/page.tsx
+++ src/app/questions/view-all-questions/page.tsx
@@ -81,7 +81,7 @@ const QuestionsPage: React.FC = () => {
 
   return (
     <div className="max-w-7xl mx-auto p-6">
-      <h1 className="text-3xl font-bold text-center text-primary mb-8">Questions</h1>
+      <h1 className="text-h2 font-bold font-subHeading text-center text-secondary-foreground">Questions</h1>
 
       <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
         {/* Sidebar for Tag Filters */}
@@ -157,7 +157,7 @@ const QuestionsPage: React.FC = () => {
                       onClick={() => handleQuestionClick(question.question_id.toString())}
                     >
                       <div className="flex justify-between items-start">
-                        <h2 className="text-xl font-semibold text-primary mb-2">{question.title}</h2>
+                      <h2 className="text-xl font-semibold text-secondary-foreground mb-2 text-balance">{question.title}</h2>
                         <div className="text-right">
                           <p className="text-base text-foreground font-medium">{question.asker_info?.username ?? 'Anonymous'}</p>
                           <p className="text-sm text-foreground font-medium">
