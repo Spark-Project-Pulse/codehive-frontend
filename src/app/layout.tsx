618854d9a5966789d851e46cbb3b581f5cfba1bf
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-10-23 11:08:30 -0400

    re-arranged globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "../styles/globals.css";
+// import localFont from "next/font/local";
+import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/universal/Navbar";
 import { UserProvider } from "@/app/contexts/UserContext";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
 
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <UserProvider>
           <Navbar />
