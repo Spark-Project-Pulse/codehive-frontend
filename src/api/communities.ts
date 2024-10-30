12d72d8b1f814d98db8de4d5013ad254275fadaf
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-10-29 21:37:35 -0400

    Implemented leaving and joining communities

--- src/api/communities.ts
+++ src/api/communities.ts
@@ -2,6 +2,100 @@
 
 import { type ApiResponse } from '@/types/Api'
 import { Community } from '@/types/Communities'
+import { getSupaUser } from '@/utils/supabase/server'
+import { UUID } from 'crypto'
+
+/**
+ * Adds a user to a community.
+ *
+ * Args:
+ *   communityId (string): The ID of the community.
+ *   userId (string): The ID of the user to add.
+ *
+ * Returns:
+ *   Promise<ApiResponse<{ message: string }>>: The success message on success or an error message on failure.
+ */
+export const addUserToCommunity = async (
+  communityId: UUID
+): Promise<ApiResponse<{ message: string }>> => {
+  try {
+    const user = await getSupaUser()
+
+    if (!user) {
+      throw new Error('User not authenticated')
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/addCommunityMember/`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ community_id: communityId, user_id: user.id }),
+      }
+    )
+
+    if (!response.ok) {
+      const errorText = await response.text()
+      throw new Error(
+        `HTTP error! Status: ${response.status}, Message: ${errorText}`
+      )
+    }
+
+    const responseData = await response.json()
+    return { errorMessage: null, data: responseData }
+  } catch (error) {
+    console.error('Error adding user to community: ', error)
+    return { errorMessage: 'Error adding user to community' }
+  }
+}
+
+/**
+ * Removes a user from a community.
+ *
+ * Args:
+ *   communityId (string): The ID of the community.
+ *   userId (string): The ID of the user to remove.
+ *
+ * Returns:
+ *   Promise<ApiResponse<{ message: string }>>: The success message on success or an error message on failure.
+ */
+export const removeUserFromCommunity = async (
+  communityId: UUID
+): Promise<ApiResponse<{ message: string }>> => {
+  try {
+    const user = await getSupaUser()
+
+    if (!user) {
+      throw new Error('User not authenticated')
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/removeCommunityMember/`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ community_id: communityId, user_id: user.id }),
+      }
+    )
+
+    if (!response.ok) {
+      const errorText = await response.text()
+      throw new Error(
+        `HTTP error! Status: ${response.status}, Message: ${errorText}`
+      )
+    }
+
+    const responseData = await response.json()
+    return { errorMessage: null, data: responseData }
+  } catch (error) {
+    console.error('Error removing user from community: ', error)
+    return { errorMessage: 'Error removing user from community' }
+  }
+}
 
 /**
  * Fetches communities with pagination, optional tag filtering, and search functionality.
@@ -16,59 +110,61 @@ import { Community } from '@/types/Communities'
  *   Promise<ApiResponse<{ communities: Community[]; totalCommunities: number }>>: The communities data on success, or an error message on failure.
  */
 export const getAllCommunities = async (
-    pageNumber: number,
-    pageSize: number,
-    selectedTags: string[],
-    searchQuery: string
-  ): Promise<ApiResponse<{ communities: Community[]; totalCommunities: number }>> => {
-    try {
-      // Build the query parameters
-      const params = new URLSearchParams()
-      params.append('page', pageNumber.toString())
-      params.append('page_size', pageSize.toString())
-      if (searchQuery.trim()) {
-        params.append('search', searchQuery.trim())
-      }
-      selectedTags.forEach((tagId) => {
-        params.append('tags', tagId)
-      })
-  
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/getAll/?${params.toString()}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-  
-      if (!response.ok) {
-        const errorText = await response.text()
-        throw new Error(
-          `HTTP error! Status: ${response.status}, Message: ${errorText}`
-        )
-      }
-  
-      const responseData = (await response.json()) as {
-        communities: Community[]
-        totalCommunities: number
-        totalPages: number
-        currentPage: number
-      }
-  
-      return {
-        errorMessage: null,
-        data: {
-          communities: responseData.communities,
-          totalCommunities: responseData.totalCommunities,
+  pageNumber: number,
+  pageSize: number,
+  selectedTags: string[],
+  searchQuery: string
+): Promise<
+  ApiResponse<{ communities: Community[]; totalCommunities: number }>
+> => {
+  try {
+    // Build the query parameters
+    const params = new URLSearchParams()
+    params.append('page', pageNumber.toString())
+    params.append('page_size', pageSize.toString())
+    if (searchQuery.trim()) {
+      params.append('search', searchQuery.trim())
+    }
+    selectedTags.forEach((tagId) => {
+      params.append('tags', tagId)
+    })
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/getAll/?${params.toString()}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
         },
       }
-    } catch (error) {
-      console.error('Error fetching communities: ', error)
-      return { errorMessage: 'Error fetching communities' }
+    )
+
+    if (!response.ok) {
+      const errorText = await response.text()
+      throw new Error(
+        `HTTP error! Status: ${response.status}, Message: ${errorText}`
+      )
     }
+
+    const responseData = (await response.json()) as {
+      communities: Community[]
+      totalCommunities: number
+      totalPages: number
+      currentPage: number
+    }
+
+    return {
+      errorMessage: null,
+      data: {
+        communities: responseData.communities,
+        totalCommunities: responseData.totalCommunities,
+      },
+    }
+  } catch (error) {
+    console.error('Error fetching communities: ', error)
+    return { errorMessage: 'Error fetching communities' }
   }
+}
 
 /**
  * Fetches a community by its ID from the backend.
@@ -80,30 +176,30 @@ export const getAllCommunities = async (
  *   Promise<ApiResponse<Community>>: The community data on success, or an error message on failure.
  */
 export const getCommunityById = async (
-    community_id: string
-  ): Promise<ApiResponse<Community>> => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/getById/${community_id}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
+  community_id: UUID
+): Promise<ApiResponse<Community>> => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/getById/${community_id}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }
-  
-      const communityData = (await response.json()) as Community
-      return { errorMessage: null, data: communityData }
-    } catch (error) {
-      console.error('Error fetching community: ', error)
-      return { errorMessage: 'Error fetching community' }
+    )
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
     }
+
+    const communityData = (await response.json()) as Community
+    return { errorMessage: null, data: communityData }
+  } catch (error) {
+    console.error('Error fetching community: ', error)
+    return { errorMessage: 'Error fetching community' }
   }
+}
 
 /**
  * Retrieves a community by their title from the backend.
@@ -115,28 +211,71 @@ export const getCommunityById = async (
  *   Promise<ApiResponse<Community>>: The community's data on success, or an error message on failure.
  */
 export const getCommunityByTitle = async (
-    title: string
-  ): Promise<ApiResponse<Community>> => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/getByTitle/${title}`,
-        {
-          method: 'GET',
-          headers: {
-            // Authorization: `Bearer ${token}`, // Uncomment if using auth
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
+  title: string
+): Promise<ApiResponse<Community>> => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/getByTitle/${title}`,
+      {
+        method: 'GET',
+        headers: {
+          // Authorization: `Bearer ${token}`, // Uncomment if using auth
+          'Content-Type': 'application/json',
+        },
       }
-  
-      const communityData = (await response.json()) as Community
-      return { errorMessage: null, data: communityData }
-    } catch (error) {
-      console.error('Error getting community: ', error)
-      return { errorMessage: 'Error getting community' }
+    )
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
     }
+
+    const communityData = (await response.json()) as Community
+    return { errorMessage: null, data: communityData }
+  } catch (error) {
+    console.error('Error getting community: ', error)
+    return { errorMessage: 'Error getting community' }
   }
+}
+
+/**
+ * Checks if a user is part of a community exists by their IDs.
+ *
+ * Args:
+ *   community_title (string): The title of the community to check.
+ *
+ * Returns:
+ *   Promise<ApiResponse<{ is_member: boolean }>>: Whether the user is part of the community on success, or an error message on failure.
+ */
+export const userIsPartOfCommunity = async (
+  community_title: string
+): Promise<ApiResponse<{ is_member: boolean }>> => {
+  try {
+    const user = await getSupaUser()
+
+    if (!user) {
+      throw new Error('User not authenticated')
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/communities/userIsPartOfCommunity/${community_title}/${user.id}`,
+      {
+        method: 'GET',
+        headers: {
+          // Authorization: `Bearer ${token}`, // Uncomment if using auth
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    // Extract the JSON data from the response
+    const responseData = (await response.json()) as { is_member: boolean }
+    return { errorMessage: null, data: { is_member: responseData.is_member } }
+  } catch (error) {
+    console.error('Error checking if user is part of community: ', error)
+    return { errorMessage: 'Error checking if user is part of community' }
+  }
+}
--- src/app/communities/[community_title]/page.tsx
+++ src/app/communities/[community_title]/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 
-import { getCommunityByTitle } from '@/api/communities'
+import {
+  addUserToCommunity,
+  getCommunityByTitle,
+  removeUserFromCommunity,
+  userIsPartOfCommunity,
+} from '@/api/communities'
 import { getAllTags } from '@/api/tags'
 import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
 import { LoadingSpinner } from '@/components/ui/loading'
+import { toast } from '@/components/ui/use-toast'
+import { ButtonWithLoading } from '@/components/universal/ButtonWithLoading'
 import { Community } from '@/types/Communities'
 import { TagOption } from '@/types/Tags'
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
@@ -17,6 +23,8 @@ export default function CommunityTitle({
   params: { community_title: string }
 }) {
   const [community, setCommunity] = useState<Community | null>(null)
+  // State for user being a member of this community, null if user is not authenticated
+  const [userIsMember, setUserIsMember] = useState<true | false | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const [tags, setTags] = useState<TagOption[]>([])
@@ -42,25 +50,84 @@ export default function CommunityTitle({
       }
     }
 
-    const fetchTags = async () => {
-        try {
-          const fetchedTags = await getAllTags()
-          setTags(fetchedTags)
-        } catch (error) {
-          console.error('Error fetching tags:', error)
+    const fetchUserIsMember = async () => {
+      try {
+        const { errorMessage, data } = await userIsPartOfCommunity(
+          params.community_title
+        )
+
+        if (!errorMessage && data) {
+          setUserIsMember(data.is_member)
+        } else {
+          console.error('Error:', errorMessage)
         }
+      } catch (error) {
+        console.error('Unexpected error:', error)
       }
+    }
+
+    const fetchTags = async () => {
+      try {
+        const fetchedTags = await getAllTags()
+        setTags(fetchedTags)
+      } catch (error) {
+        console.error('Error fetching tags:', error)
+      }
+    }
 
     void fetchCommunity()
+    void fetchUserIsMember()
     void fetchTags()
   }, [params.community_title])
 
   const handleJoinCommunity = async () => {
     try {
-        
-      } catch (error) {
-
+      if (!community) {
+        console.error('Community is null')
+        return
       }
+
+      const { errorMessage, data } = await addUserToCommunity(
+        community?.community_id
+      )
+
+      if (!errorMessage && data) {
+        setUserIsMember(true)
+        toast({
+          title: 'Success!',
+          description: `You are now a part of the ${community.title} community`,
+        })
+      } else {
+        console.error('Error:', errorMessage)
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error)
+    }
+  }
+
+  const handleLeaveCommunity = async () => {
+    try {
+      if (!community) {
+        console.error('Community is null')
+        return
+      }
+
+      const { errorMessage, data } = await removeUserFromCommunity(
+        community?.community_id
+      )
+
+      if (!errorMessage && data) {
+        setUserIsMember(false)
+        toast({
+          title: 'Success!',
+          description: `You have left the ${community.title} community`,
+        })
+      } else {
+        console.error('Error:', errorMessage)
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error)
+    }
   }
 
   // Conditional rendering for loading state
@@ -68,29 +135,46 @@ export default function CommunityTitle({
     return <LoadingSpinner />
   }
 
-  return(
+  return (
     <div className="container mx-auto px-4 py-8">
-          <div className="flex flex-col md:flex-row items-start gap-6 mb-8">
-        <Avatar className="w-24 h-24">
-          <AvatarImage src={community?.avatar_url ?? '/default-community-avatar.png'} alt="Community avatar" />
+      <div className="mb-8 flex flex-col items-start gap-6 md:flex-row">
+        <Avatar className="h-24 w-24">
+          <AvatarImage
+            src={community?.avatar_url ?? '/default-community-avatar.png'}
+            alt="Community avatar"
+          />
         </Avatar>
         <div className="flex-1">
-          <h1 className="text-3xl font-bold mb-2">{community?.title}</h1>
-          <p className="text-muted-foreground mb-4">{community?.description}</p>
-          <div className="flex flex-wrap gap-2 mb-4">
-          {community?.tags?.map((tagId) => {
-            const tag = tags.find((t) => t.value === tagId)
-            return tag ? <Badge variant="secondary">{tag.label}</Badge> : null // Handle cases where tag is not found
-          })}
+          <h1 className="mb-2 text-3xl font-bold">{community?.title}</h1>
+          <p className="mb-4 text-muted-foreground">{community?.description}</p>
+          <div className="mb-4 flex flex-wrap gap-2">
+            {community?.tags?.map((tagId) => {
+              const tag = tags.find((t) => t.value === tagId)
+              return tag ? <Badge variant="secondary">{tag.label}</Badge> : null // Handle cases where tag is not found
+            })}
           </div>
           <div className="flex items-center gap-4 text-sm text-muted-foreground">
             <span className="flex items-center gap-1">
-              <Users className="w-4 h-4" />
+              <Users className="h-4 w-4" />
               {community?.member_count.toLocaleString()} members
             </span>
           </div>
         </div>
-        <Button onClick={handleJoinCommunity}>Join Community</Button>
+        {userIsMember === false && (
+          <ButtonWithLoading
+            onClick={handleJoinCommunity}
+            buttonText="Join Community"
+            buttonType="button"
+          />
+        )}
+
+        {userIsMember === true && (
+          <ButtonWithLoading
+            onClick={handleLeaveCommunity}
+            buttonText="Leave Community"
+            buttonType="button"
+          />
+        )}
       </div>
     </div>
   )
