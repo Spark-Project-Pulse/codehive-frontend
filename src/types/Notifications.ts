53e4dfe5e196c25a70dfa37279703121f0cdf3c2
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-10 17:21:21 -0500

    enhanced notification type

--- src/types/Notifications.ts
+++ src/types/Notifications.ts
@@ -1,6 +1,30 @@
 import { type UUID } from 'crypto'
+import { type User } from '@/types/Users'
+import { type Question } from '@/types/Questions'
+import { type Answer } from '@/types/Answers'
+import { type Comment } from '@/types/Comments'
 
 export interface Notification {
-  question_id: UUID
-  
+  notification_id: UUID
+  recipient: UUID
+  notification_type: NotificationType
+  message: string
+  read: boolean
+  created_at: Date
+  recipient_info: User | null
+  question_info: Question | null
+  answer_info: Answer | null
+  comment_info: Comment | null
+  actor_info : User | null
+  question: UUID | null
+  answer: UUID | null
+  comment: UUID | null
+  actor: UUID | null
 }
+
+type NotificationType =
+  | "question_answered"
+  | "answer_commented"
+  | "question_upvoted"
+  | "answer_accepted"
+  | "mention"
