d3a125af5d0ac392e7555642687d954214b394d2
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-24 15:02:39 -0500

    integrated bento card and adjusted stylings accordingly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client"
 
+import Features from "@/components/pages/home/Features";
 import Hero from "@/components/pages/home/Hero";
 
 export default function Home() {
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
+    <div className="flex flex-col min-h-screen py-12 md:py-24 lg:py-32 xl:py-48">
+      <main className="flex-grow space-y-12 md:space-y-24 lg:space-y-32">
         <Hero />
-        {/* <Features /> */}
+        <Features />
         {/* <Testimonials /> */}
         {/* <FAQ /> */}
       </main>
