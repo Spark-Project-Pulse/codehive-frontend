5aac0949ea0cbb7e752a894cfcbc8f806a5254b6
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-24 15:40:24 -0500

    fixed more icons

--- src/components/pages/home/Features.tsx
+++ src/components/pages/home/Features.tsx
@@ -1,72 +1,65 @@
 import React from 'react'
 
-import {
-  BellIcon,
-  FileTextIcon,
-  GitHubLogoIcon,
-  CodeIcon, 
-} from "@radix-ui/react-icons";
-
-import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
-import { Hexagon } from 'lucide-react';
+import { BellIcon, GitHubLogoIcon, CodeIcon } from '@radix-ui/react-icons'
+import { BentoCard, BentoGrid } from '@/components/ui/bento-grid'
+import { Hexagon, MessageCircleQuestionIcon } from 'lucide-react'
 
 const features = [
   {
-    Icon: FileTextIcon,
-    name: "Find, Ask, and Answer Questions",
-    description: "Get personal feedback on your work.",
-    href: "/",
-    cta: "Learn more",
+    Icon: MessageCircleQuestionIcon,
+    name: 'Find, Ask, and Answer Questions',
+    description: 'Get personal feedback on your work.',
+    href: '/',
+    cta: 'Learn more',
     background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
+    className: 'lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3',
   },
   {
     Icon: GitHubLogoIcon,
-    name: "GitHub Integration",
-    description: "Easily sync your projects with GitHub repos.",
-    href: "/",
-    cta: "Learn more",
+    name: 'GitHub Integration',
+    description: 'Easily sync your projects with GitHub repos.',
+    href: '/',
+    cta: 'Learn more',
     background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+    className: 'lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3',
   },
   {
     Icon: CodeIcon,
-    name: "AI Powered Code Review",
-    description: "Get real-time feedback on your code.",
-    href: "/",
-    cta: "Learn more",
+    name: 'AI Powered Code Review',
+    description: 'Get real-time feedback on your code.',
+    href: '/',
+    cta: 'Learn more',
     background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
+    className: 'lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4',
   },
   {
     Icon: Hexagon,
-    name: "Hives",
-    description: "Find communities with similar interests.",
-    href: "/",
-    cta: "Learn more",
+    name: 'Hives',
+    description: 'Find communities with similar interests.',
+    href: '/',
+    cta: 'Learn more',
     background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
+    className: 'lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2',
   },
   {
     Icon: BellIcon,
-    name: "Notifications",
-    description:
-      "Get notified when your question is answered.",
-    href: "/",
-    cta: "Learn more",
+    name: 'Notifications',
+    description: 'Get notified when your question is answered.',
+    href: '/',
+    cta: 'Learn more',
     background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
+    className: 'lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4',
   },
-];
+]
 
 export default function Features() {
   return (
-    <section className="w-full  px-20">
+    <section className="w-full px-20">
       <BentoGrid className="lg:grid-rows-3">
         {features.map((feature) => (
           <BentoCard key={feature.name} {...feature} />
         ))}
       </BentoGrid>
     </section>
-  );
+  )
 }
