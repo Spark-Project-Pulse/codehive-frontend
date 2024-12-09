566044ea5b31f83e74142bff7946afca8212528b
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-12-09 12:57:56 -0500

    Fixed hive bg in dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,27 @@ import Features from '@/components/pages/home/Features'
 import Hero from '@/components/pages/home/Hero'
 import Testimonials from '@/components/pages/home/Testimonials'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   const { resolvedTheme } = useTheme()
+  const [isThemeResolved, setIsThemeResolved] = useState(false)
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      setIsThemeResolved(true)
+    }
+  }, [resolvedTheme])
 
   return (
     <div className="-ml-6 -mr-6 -mt-12">
       <div
         className={
-          resolvedTheme === 'dark' ? 'hexagonal-bg-dark' : 'hexagonal-bg-light'
+          isThemeResolved
+            ? resolvedTheme === 'dark'
+              ? 'hexagonal-bg-dark'
+              : 'hexagonal-bg-light'
+            : '' // Default background during initial load
         }
       >
         <div className="flex min-h-screen flex-col py-12 md:py-24 lg:py-32 xl:py-48">
